420bbfef630e41ba2b64382c0dc84769c33f859a
chengkoon <chengkoon@gmail.com>
2017-02-13 12:20:14 +0800

    second commit

--- db/migrate/20170213041135_create_followings.rb
+++ db/migrate/20170213041135_create_followings.rb
@@ -2,7 +2,7 @@ class CreateFollowings < ActiveRecord::Migration[5.0]
   def change
     create_table :followings do |t|
         t.integer :user_id
-        t.integer :follower_id, from_class 'User'
+        t.integer :follower_id
 
       t.timestamps
     end
