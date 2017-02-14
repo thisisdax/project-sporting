62a2555b577a1cd2b5fd6661f8f342ef766de961
chengkoon <chengkoon@gmail.com>
2017-02-14 14:22:01 +0800

    comment feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  # get 'comments/create'
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :events
+  resources :events do
+    resources :comments
+  end
+
 
   #contact is just a test controller because cannot render view without it
 end
