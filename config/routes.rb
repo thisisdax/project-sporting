6badabdd94b356dc1fa507fe19a2561ec55cd582
Agnes <Agnes@Agness-MacBook-Pro.local>
2017-02-14 14:32:29 +0800

    added nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :events
+  root to: 'events#index'
 
   #contact is just a test controller because cannot render view without it
 end
